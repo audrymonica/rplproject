65276e20d3ce8c5b5a79b7281faaebaf08f7bca0
audrymonica <123612256+audrymonica@users.noreply.github.com>
2023-12-04 14:49:09 +0700

    Add files via upload

---
+++ DataAnggotaWJMA/anggotawjma.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 14, 2023 at 03:19 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `anggotawjma`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dataanggota`
+--
+
+CREATE TABLE `dataanggota` (
+  `nama` varchar(255) NOT NULL,
+  `level` varchar(25) NOT NULL,
+  `kdanggota` varchar(25) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `umur` int(99) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
